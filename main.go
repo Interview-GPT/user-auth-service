c56b2a7a52fb4f1562ff64eef5ef7500a7dfd7f3
Joseph Jophy <79068629+josephjophy@users.noreply.github.com>
2023-10-25 10:43:48 -0500

    test loadEnvVariables.go initializer and set up gin web server

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+    "github.com/gin-gonic/gin"
+	"github.com/Interview-GPT/user-auth-service/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 
 func main(){
 	fmt.Println("Hello, world!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
